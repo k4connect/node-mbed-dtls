6ed835b40a0dd6b02c63cc22bd3b48ec33220f98
Sam Thompson <git@samt.us>
2019-09-16 13:04:35 -0700

    Add DtlsSocket tests

--- test/unit/DtlsSocket.test.js
+++ test/unit/DtlsSocket.test.js
@@ -1,17 +1,55 @@
 'use strict';
 
+const chai = require('chai')
+const fs = require('fs');
 const path = require('path');
 const should = require('should');
 const sinon = require('sinon');
 const assert = require('assert');
 
-const mbed = require('bindings')('node_mbed_dtls.node');
+const expect = chai.expect;
 
-const opts = {
-	cert: path.join(__dirname, 'test/public.der'),
-	key: path.join(__dirname, 'test/private.der')
-};
+const serverKey = fs.readFileSync(path.join(__dirname, '..', 'private.der'));
+const { DtlsSocket, DtlsServer } = require('bindings')('node_mbed_dtls.node');
+
 
 describe('DtlsSocket', function() {
+	describe('exports', function () {
+		it('should exist', function () {
+			expect(DtlsSocket).to.be.a('function');
+		});
 
+		it('should be named correctly', function () {
+			expect(DtlsSocket.name).to.equal('DtlsSocket');
+		});
+	});
+
+	describe('constructor', function () {
+		const ip = '123.45.67.89';
+		let server;
+		let sendCb;
+		let handshakeCb;
+		let errorCb;
+		let sessResumeCb;
+
+		beforeEach(function () {
+			server = new DtlsServer(serverKey);
+			sendCb = sinon.stub();
+			handshakeCb = sinon.stub();
+			errorCb = sinon.stub();
+			sessResumeCb = sinon.stub();
+		});
+
+		it('should throw if constructed with no arguments', function () {
+			expect(() => { new DtlsSocket() }).to.throw();
+		});
+
+		it('should throw if called as a function', function () {
+			expect(() => { DtlsSocket(key) }).to.throw();
+		});
+
+		it('should construct correctly given all arguments', function () {
+			expect(new DtlsSocket(server, '123.45.67.89', sendCb, handshakeCb, errorCb, sessResumeCb)).to.be.instanceOf(DtlsSocket);
+		});
+	});
 });
